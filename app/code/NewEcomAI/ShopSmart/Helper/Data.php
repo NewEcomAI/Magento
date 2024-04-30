34e3b0aa30dd9aa9e623bd2aff3ac4bfa10a4997
Ameer Hamza <ameer.hamza@nestosh.com>
2024-04-30 17:34:44 +0500

    NECOMAI-9 Add Config value method

--- app/code/NewEcomAI/ShopSmart/Helper/Data.php
+++ app/code/NewEcomAI/ShopSmart/Helper/Data.php
@@ -16,6 +16,16 @@ use Magento\Store\Model\StoreManagerInterface;
 class Data extends AbstractHelper
 {
 
+    const MODULE_ENABLE = 'shop_smart/general_account_configuration/enable';
+    const SHOP_SMART_MODE = 'shop_smart/general_account_configuration/shop_smart_mode';
+    const SHOP_SMART_USER_ID = 'shop_smart/general_account_configuration/user_id<';
+    const SHOP_SMART_USER_NAME = 'shop_smart/general_account_configuration/user_name';
+    const SHOP_SMART_USER_PASSWORD = 'shop_smart/general_account_configuration/user_password';
+    const SHOP_SMART_AB_TESTING = 'shop_smart/general_account_configuration/ab_testing';
+    const SHOP_SMART_CATALOG_SYNC_DATE = 'shop_smart/general_catalog_sync/catalog_sync_date';
+    const SHOP_SMART_CATALOG_SYNC_BUTTON = 'shop_smart/general_catalog_sync/catalog_sync_button';
+    const SHOP_SMART_MAPPING = 'shop_smart/general_product_attribute_mapping/mapping';
+
     /**
      * @var LoggerInterface
      */
@@ -62,15 +72,16 @@ class Data extends AbstractHelper
      * @param Context $context
      */
     public function __construct(
-        LoggerInterface            $logger,
-        RedirectInterface          $redirect,
-        StoreManagerInterface      $storeManager,
-        Session                    $customerSession,
-        CheckoutSession            $checkoutSession,
-        ProductRepository          $productRepository,
-        ScopeConfigInterface       $scopeConfigInterface,
-        Context                    $context
-    ) {
+        LoggerInterface       $logger,
+        RedirectInterface     $redirect,
+        StoreManagerInterface $storeManager,
+        Session               $customerSession,
+        CheckoutSession       $checkoutSession,
+        ProductRepository     $productRepository,
+        ScopeConfigInterface  $scopeConfigInterface,
+        Context               $context
+    )
+    {
         $this->logger = $logger;
         $this->redirect = $redirect;
         $this->storeManager = $storeManager;
@@ -79,6 +90,88 @@ class Data extends AbstractHelper
         $this->productRepository = $productRepository;
         $this->scopeConfigInterface = $scopeConfigInterface;
         parent::__construct($context);
+    }
+
+
+    /**
+     * @param null $storeId
+     * @return mixed
+     */
+    public function isEnable($storeId = null)
+    {
+        return $this->scopeConfig->getValue(self::MODULE_ENABLE, ScopeInterface::SCOPE_STORE, $storeId);
+    }
+
+    /**
+     * @param $storeId
+     * @return mixed
+     */
+    public function getShopSmartMode($storeId = null)
+    {
+        return $this->scopeConfig->getValue(self::SHOP_SMART_MODE, $storeId);
+    }
+
+    /**
+     * @param $storeId
+     * @return mixed
+     */
+    public function getShopSmartUserId($storeId = null)
+    {
+        return $this->scopeConfig->getValue(self::SHOP_SMART_USER_ID, $storeId);
+    }
+
+    /**
+     * @param $storeId
+     * @return mixed
+     */
+    public function getShopSmartUserName($storeId = null)
+    {
+        return $this->scopeConfig->getValue(self::SHOP_SMART_USER_NAME, $storeId);
+    }
+
+    /**
+     * @param $storeId
+     * @return mixed
+     */
+    public function getShopSmartUserPassword($storeId = null)
+    {
+        return $this->scopeConfig->getValue(self::SHOP_SMART_USER_PASSWORD, $storeId);
+    }
+
+    /**
+     * @param $storeId
+     * @return mixed
+     */
+    public function getShopSmartABTesting($storeId = null)
+    {
+        return $this->scopeConfig->getValue(self::SHOP_SMART_AB_TESTING, $storeId);
+    }
+
+    /**
+     * @param $storeId
+     * @return mixed
+     */
+    public function getShopSmartCatalogSyncDate($storeId = null)
+    {
+        return $this->scopeConfig->getValue(self::SHOP_SMART_CATALOG_SYNC_DATE, $storeId);
+    }
+
+    /**
+     * @param $storeId
+     * @return mixed
+     */
+    public function getShopSmartProductAttribute($storeId = null)
+    {
+        return $this->scopeConfig->getValue(self::SHOP_SMART_MAPPING, $storeId);
+    }
+
+    /**
+     * @param $storeId
+     * @return mixed
+     */
+    public function getShopSmartCatalogSync($storeId = null)
+    {
+        return $this->scopeConfig->getValue(self::SHOP_SMART_CATALOG_SYNC_BUTTON, $storeId);
     }
 
     /**
@@ -100,13 +193,6 @@ class Data extends AbstractHelper
         }
     }
 
-    /**
-     * @return mixed|null
-     */
-    public function getPopUpPosition()
-    {
-        return $this->getConfigValue('shop_smart/general/popup');
-    }
 }
 
 
