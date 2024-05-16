4a2ca65a3d79322ebc0151495d03bcb41b747c92
Ayesha Zubair <ayesha.zubair@nestosh.com>
2024-05-16 21:15:32 +0500

    NECOMAI-21 NECOMAI25 Created a global template for the decide widget, styled the child templates, implemented the JS function for after search, removed the default scrollbar from the Decide and Discover templates, implemented a JS function to show the scrollbar when the content exceeds the popup container.

--- app/code/NewEcomAI/ShopSmart/view/frontend/requirejs-config.js
+++ app/code/NewEcomAI/ShopSmart/view/frontend/requirejs-config.js
@@ -2,7 +2,8 @@ var config = {
     map: {
         '*': {
             customNewcom: 'NewEcomAI_ShopSmart/js/components/newcomCustom',
-            productGridNewcom: 'NewEcomAI_ShopSmart/js/components/newcomProductGrid'
+            productGridNewcom: 'NewEcomAI_ShopSmart/js/components/newcomProductGrid',
+            decideTemplateNewcom: 'NewEcomAI_ShopSmart/js/components/newcomDecideTemplate'
         }
     }
 };
--- app/code/NewEcomAI/ShopSmart/view/frontend/web/js/components/newcomCustom.js
+++ app/code/NewEcomAI/ShopSmart/view/frontend/web/js/components/newcomCustom.js
@@ -9,16 +9,17 @@ define([
               var newEcomClickedText = $(this).text().trim(); 
               $('#NewEcomAi-question').val(newEcomClickedText);
           });
+
           //  JS condition for discover template postion
-          if ($('.newSmartShopDiscover').hasClass('newcomLeftSide')) {
+          if ($('.newSmartShopDiscover, .newSmartShopDecide').hasClass('newcomLeftSide')) {
               $('#newcomLeftBtn').on('click', function() {
                 $('#newcom-popup').css('display', 'block');
               });
-          } else if ($('.newSmartShopDiscover').hasClass('newcomRightSide')) {
+          } else if ($('.newSmartShopDiscover, .newSmartShopDecide').hasClass('newcomRightSide')) {
             $('#newcomRightBtn').on('click', function() {
               $('#newcom-popup').css('display', 'block');
             });
-          } else if ($('.newSmartShopDiscover').hasClass('newcomProductGrid')) {
+          } else if ($('.newSmartShopDiscover, .newSmartShopDecide').hasClass('newcomProductGrid')) {
             $('#newcom-popup').css('display', 'block');
           } else {
             $('#newcom-popup').css('display', 'block');
@@ -27,6 +28,32 @@ define([
           $('#newComclose').click(function() {
             $('#newcom-popup').css('display', 'none');
           });
+
+          // Function to show the scrollbar when the content is greater than popup container
+          function newcomCheckOverflow() {
+            var $container = $('.js-newcom-popup-content');
+            if ($container[0].scrollHeight > $container.innerHeight()) {
+                $container.addClass('show-scrollbar');
+            } else {
+                $container.removeClass('show-scrollbar');
+            }
+          }
+
+          newcomCheckOverflow(); // Initial check on document ready
+          $(window).resize(newcomCheckOverflow); // Check on window resize
+
+          // MutationObserver to detect changes in the container
+          var observer = new MutationObserver(function(mutations) {
+              mutations.forEach(function(mutation) {
+                newcomCheckOverflow();
+              });
+          });
+
+          // Configuration of the observer
+          var config = { childList: true, subtree: true, characterData: true };
+
+          // Pass in the target node, as well as the observer options
+          observer.observe(document.querySelector('.js-newcom-popup-content'), config);
       });
     }
 });
