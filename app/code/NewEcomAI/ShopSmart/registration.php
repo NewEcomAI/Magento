188ca312469493a495979696eebe891bf451740a
Maham <a2b-mira_dev@miracommerce.com>
2024-08-22 14:57:31 +0500

    Removing app/code from composer branch

--- app/code/NewEcomAI/ShopSmart/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'NewEcomAI_ShopSmart', __DIR__);
--- app/code/NewEcomAI/ShopSmart/view/frontend/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    map: {
-        '*': {
-            customNewcom: 'NewEcomAI_ShopSmart/js/components/newcomCustom',
-            discoverSearchNewEcom: 'NewEcomAI_ShopSmart/js/components/newEcomDiscoverSearch',
-            decideSearchNewEcom: 'NewEcomAI_ShopSmart/js/components/newEcomDecideSearch'
-        }
-    }
-};
